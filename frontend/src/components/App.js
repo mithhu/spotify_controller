1d3ccd5845d5eaa025125d13f300052462cf9dad
Mahamudul Hasan <mithhu@Mahamuduls-MacBook-Pro.local>
2022-12-31 03:53:19 +0800

    added react router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div>
-        <h1>React App</h1>
+        <HomePage />
       </div>
     );
   }
